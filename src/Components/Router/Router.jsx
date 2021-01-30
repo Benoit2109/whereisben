82509ce86f825acb46cd9f1e76abddcd8697c004
Benoit LE BOURDONNEC <benoitlebourdonnec@MacBook-Pro-2.local>
2021-01-30 13:33:21 +0100

    init project

---
+++ src/Components/Router/Router.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+
+export default function Root() {
+    return (
+        <div>
+            
+        </div>
+    )
+}

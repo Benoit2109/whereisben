af28f7d784267e75621f6e81c10d335e6b5d8eb2
Benoit LE BOURDONNEC <benoitlebourdonnec@MacBook-Pro-2.local>
2021-02-03 11:33:38 +0100

    autocomplete works needs to fix ternary condition

--- src/Components/Autocomplete/Autocomplete.jsx
+++ src/Components/Autocomplete/Autocomplete.jsx
@@ -1,31 +1,42 @@
-import React, { useEffect } from "react";
+import React from "react";
 import axios from "axios";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
-function Autocomplete({ newcity,latitude, setLatitude,longitude, setLongitude }) {
+import styles from './Autocomplete.module.css';
+
+function Autocomplete({ newcity, setLatitude, setLongitude }) {
   const { country_name, city_name } = newcity;
+  
 
-  const country = country_name.toLowerCase();
-  const city = city_name.toLowerCase();
-
+  const country = country_name.toLowerCase().split(" ").join("+");
+  const city = city_name.toLowerCase().split(" ").join("+");
   const Url = "https://nominatim.openstreetmap.org/search?";
 
-  useEffect(() => {
+  const fetchCity = () => {
     axios
       .get(`${Url}q=${country},+${city}&format=geojson`)
       .then((res) => res.data.features[0])
       .then((res) => {
-        setLatitude(res.geometry.coordinates[0]);
-        setLongitude(res.geometry.coordinates[1]);
+        if (res.geometry.coordinates[0].length > 0 || res.geometry.coordinates[1].length>0) {
+          setLatitude(res.geometry.coordinates[0]);
+          setLongitude(res.geometry.coordinates[1]);
+        } else {
+            displayError();
+        }
       });
-  }, [country, city, setLatitude, setLongitude]);
+  };
+
+  const displayError = () => {
+    alert(
+      "Veuillez préciser votre recherche en respectant les règles d'écriture"
+    );
+  };
 
   return (
     <div>
-      <ul>
-        <li>{latitude}</li>
-        <li>{longitude}</li>
-      </ul>
+      <button className={styles.auto_button}type="button" onClick={fetchCity}>
+        Rechercher
+      </button>
     </div>
   );
 }
@@ -33,7 +44,7 @@ function Autocomplete({ newcity,latitude, setLatitude,longitude, setLongitude })
 export default Autocomplete;
 
 Autocomplete.propTypes = {
-    newcity: PropTypes.object.isRequired,
-    setLongitude: PropTypes.number.isRequired,
-    setLatitude: PropTypes.number.isRequired,
-}
+  newcity: PropTypes.object.isRequired,
+  setLongitude: PropTypes.number.isRequired,
+  setLatitude: PropTypes.number.isRequired,
+};

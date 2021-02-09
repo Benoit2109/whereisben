6c829a04019b8155bdecbbb4c77a3e17f0f0d1b3
Benoit LE BOURDONNEC <benoitlebourdonnec@MacBook-Pro-2.local>
2021-02-09 13:23:48 +0100

    resolved problem

---
+++ src/Components/Searchbar/Searchbar.jsx
@@ -0,0 +1,20 @@
+import React, {useState} from "react";
+import styles from './SearchBar.module.css';
+
+function SearchBar() {
+    const [citysearch, setCitysearch] = useState("");
+
+    const handleChange = (e) => {
+        setCitysearch(e.target.value)
+    }
+
+  return (
+    <div className={styles.searchbar_wrapper}>
+      <label htmlFor="searchcity">
+        <input type="search" className={styles.searchbar} value={citysearch} onChange={handleChange} placeholder='Rechercher un pays ou une ville...'/>
+      </label>
+    </div>
+  );
+}
+
+export default SearchBar;
